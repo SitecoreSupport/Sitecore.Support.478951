a31a709539662377b7c49c60941dbcb720cf8f0a
NhatTanVu <onchua2006@gmail.com>
2017-03-03 18:31:01 +0800

    Commit patch

---
+++ src/Sitecore.Support.478951/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.478951")]
+[assembly: AssemblyProduct("Sitecore.Support.478951")]
+[assembly: ComVisible(false)]
