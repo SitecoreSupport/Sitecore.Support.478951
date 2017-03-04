46401c996a183a1dbf01014f99e3bb55bd49a0a3
NhatTanVu <onchua2006@gmail.com>
2017-03-04 23:02:10 +0800

    Implement details.

---
+++ src/Sitecore.Support.478951/MoveToSent.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Modules.EmailCampaign.Core;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter;
+using Sitecore.Modules.EmailCampaign.Messages;
+using Sitecore.SecurityModel;
+using Sitecore.StringExtensions;
+using System;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter
+{
+    public class MoveToSent
+    {
+        public void Process(DispatchNewsletterArgs args)
+        {
+            if (args.SendingAborted && !string.IsNullOrEmpty(args.ProcessData.Errors))
+            {
+                return;
+            }
+
+            if (!args.IsTestSend && !args.DedicatedInstance && (args.SendingAborted || args.RequireFinalMovement))
+            {
+                if (args.Message.MessageType != MessageType.Trickle)
+                {
+                    using (new SecurityDisabler())
+                    {
+                        args.Message.Source.ReleaseRelatedItems();
+                        args.Message.Source.State = MessageState.Sent;
+                        args.Message.Source.EndTime = System.DateTime.Now;
+                        ExternalLinks.DatabaseToItem(args.Message);
+                    }
+                }
+            }
+        }
+    }
+}
